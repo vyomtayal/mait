3c140b0e096afc814f31dc1ed5da9d50c287290a
vyomtayal <vyomtayal5@gmail.com>
2026-08-31 15:15:55 +0530

    Add files via upload

---
+++ MARKS.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+ int marks;
+ printf("Enter the marks of the student: ");
+ scanf("%d", &marks);
+
+ if (marks>=90){
+ printf("\ngrade:A+");
+ }
+ else if(marks>=80){
+ printf("\ngrade:A");
+ }
+ else if(marks>=60){
+ printf("\ngrade:B");
+ }
+ else if(marks>=40){
+ printf("\ngrade:c");
+ }
+ else if(marks<40){
+ printf("\nfailed");
+ }
+ else{
+ printf("invalid");
+ }
+ return 0;
+}
